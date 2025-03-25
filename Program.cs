7c35a79e74656ab6c91ff456e1dba7b05a0a0708
umikonashimi <margoszr7@gmail.com>
2025-03-25 17:37:17 +0300

    4

--- Program.cs
+++ Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("Im D! And i love N! My friend O loves M))");
 Console.WriteLine("But i dont love V!!!");
 Console.WriteLine("And O dont love O and A!!!");
 Console.WriteLine("We will go to Krasnodar with O)");
+Console.WriteLine("And with V and E!");
