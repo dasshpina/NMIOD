3ee06ddc848c1bd7966c3633c81bd329df211ec8
umikonashimi <margoszr7@gmail.com>
2025-03-25 17:31:23 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Im D! And i love N! My friend O loves M))");
+Console.WriteLine("But i dont love V!!!");
